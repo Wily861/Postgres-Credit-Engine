cd50759532cd5e6e81b7c91f9afe950125351222
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 16:36:48 -0500

    Update 06_create_amortization_schedule_table.sql

--- scripts/06_create_amortization_schedule_table.sql
+++ scripts/06_create_amortization_schedule_table.sql
@@ -1,1 +1,8 @@
-
+-- 6. Crear tabla de pagos
+CREATE TABLE IF NOT EXISTS core.pagos (
+  pago_id BIGSERIAL PRIMARY KEY,
+  schedule_id BIGINT NOT NULL REFERENCES core.payment_schedule(schedule_id),
+  fecha_pago TIMESTAMPTZ NOT NULL,
+  monto NUMERIC(12,2) NOT NULL,
+  medio TEXT
+);
