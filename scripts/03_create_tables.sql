3313bc4d42d7599b58c2835b291929bb650936f9
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 16:31:07 -0500

    Update 03_create_tables.sql

--- scripts/03_create_tables.sql
+++ scripts/03_create_tables.sql
@@ -1,1 +1,9 @@
-
+-- 2. Crear tabla de clientes
+CREATE TABLE IF NOT EXISTS core.clientes (
+  cliente_id BIGSERIAL PRIMARY KEY,
+  tipo_doc   TEXT NOT NULL,
+  num_doc    TEXT NOT NULL,
+  nombre     TEXT NOT NULL,
+  ciudad     TEXT,
+  UNIQUE (tipo_doc, num_doc)
+);
